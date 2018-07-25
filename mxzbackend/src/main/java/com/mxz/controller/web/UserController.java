d8e330f87ab0ed8ba6d5e39c9376cd0a5a9a55a2
mxz <836323493@qq.com>
2018-07-26 01:50:41 +0800

    new

---
+++ mxzbackend/src/main/java/com/mxz/controller/web/UserController.java
@@ -0,0 +1,19 @@
+package com.mxz.controller.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+*@Description 
+*@author mxz
+*2018-07-26
+**/
+@RequestMapping("user")
+@RestController
+public class UserController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
